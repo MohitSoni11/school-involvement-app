aa702aa1844859f14facc075137fdb98ad2240c2
Mohit Soni <92624795+MohitSoni11@users.noreply.github.com>
2023-04-10 21:09:06 -0700

    Added home page and connected website to it

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def signup():
             'password': request.form['password'],
             'name': request.form['name']}
     
-    if (check_json(request.form['email'], request.form['password'])):
+    if (len(check_json(data['email'], data['password'])) != 0):
       return render_template('signup.html', repeat=True)
     else:
       write_json(data)
@@ -27,13 +27,25 @@ def signup():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
   if (request.method == 'POST'):
-    if (check_json(request.form['email'], request.form['password'])):
+    email = request.form['email']
+    password = request.form['password']
+    name = check_json(email, password)
+    if (len(name) != 0):
+      global user_email
+      user_email = email
+      global user_password
+      user_password = password
+      global user_name
+      user_name = name
       return redirect('/home')
     else:
       return render_template('login.html', incorrect=True)
   else:
     return render_template('login.html')
   
+@app.route('/home')
+def home():
+  return render_template('home.html', name=user_name)
 
 ####################### Helper Methods #######################
   
@@ -50,5 +62,5 @@ def check_json(email, password, filename='data.json'):
     user_data = file_data['users']
     for user in user_data:
       if (user['email'] == email and user['password'] == password):
-        return True
-    return False
+        return user['name']
+    return ''
