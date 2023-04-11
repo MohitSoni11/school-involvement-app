baf5e0f39b0f8180d8e4f55e20a1e4fb2bd20e8c
Mohit Soni <92624795+MohitSoni11@users.noreply.github.com>
2023-04-10 20:18:30 -0700

    Done with login/signup functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(
   __name__,
@@ -6,6 +7,48 @@ app = Flask(
   static_folder='static'
   )
 
-@app.route('/')
-def home():
-  return render_template('login.html')
+####################### Routing #######################
+
+@app.route('/', methods=['POST', 'GET'])
+def signup():
+  if (request.method == 'POST'):
+    data = {'email': request.form['email'], 
+            'password': request.form['password'],
+            'name': request.form['name']}
+    
+    if (check_json(request.form['email'], request.form['password'])):
+      return render_template('signup.html', repeat=True)
+    else:
+      write_json(data)
+      return redirect('/login')
+  else:
+    return render_template('signup.html')
+  
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+  if (request.method == 'POST'):
+    if (check_json(request.form['email'], request.form['password'])):
+      return 'Logged In!'
+    else:
+      return 'Incorrect credentials!'
+  else:
+    return render_template('login.html')
+  
+
+####################### Helper Methods #######################
+  
+def write_json(new_data, filename='data.json'):
+  with open(filename, 'r+') as file:
+    file_data = json.load(file)
+    file_data['users'].append(new_data)
+    file.seek(0)
+    json.dump(file_data, file, indent=4)
+    
+def check_json(email, password, filename='data.json'):
+  with open(filename, 'r+') as file:
+    file_data = json.load(file)
+    user_data = file_data['users']
+    for user in user_data:
+      if (user['email'] == email and user['password'] == password):
+        return True
+    return False
