dbe634b0824a3492f00915d937cac518d2643804
Mohit Soni <92624795+MohitSoni11@users.noreply.github.com>
2023-04-11 19:37:56 -0700

    Student sign up working

--- app.py
+++ app.py
@@ -9,23 +9,14 @@ app = Flask(
 
 ####################### Routing #######################
 
-@app.route('/', methods=['POST', 'GET'])
-def signup():
-  if (request.method == 'POST'):
-    data = {'email': request.form['email'], 
-            'password': request.form['password'],
-            'name': request.form['name']}
-    
-    if (len(check_json(data['email'], data['password'])) != 0):
-      return render_template('signup.html', repeat=True)
-    else:
-      write_json(data)
-      return redirect('/login')
-  else:
-    return render_template('signup.html')
-  
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+# Intro screen
+@app.route('/')
+def intro():
+  return render_template('intro.html')
+
+# Admin Login Screen
+@app.route('/admin-login', methods=['POST', 'GET'])
+def admin_login():
   if (request.method == 'POST'):
     email = request.form['email']
     password = request.form['password']
@@ -37,26 +28,80 @@ def login():
       user_password = password
       global user_name
       user_name = name
-      return redirect('/home')
+      return redirect('/admin-home')
     else:
-      return render_template('login.html', incorrect=True)
+      return render_template('admin-login.html', incorrect=True)
   else:
-    return render_template('login.html')
-  
-@app.route('/home')
-def home():
-  return render_template('home.html', name=user_name)
+    return render_template('admin-login.html')
+
+# Admin Signup Screen
+@app.route('/admin-signup', methods=['POST', 'GET'])
+def admin_signup():
+  if (request.method == 'POST'):
+    data = {'email': request.form['email'], 
+            'password': request.form['password'],
+            'name': request.form['name']}
+    
+    if (len(check_json(data['email'], data['password'])) != 0):
+      return render_template('admin-signup.html', repeat=True)
+    else:
+      write_json(data)
+      return redirect('/admin-login')
+  else:
+    return render_template('admin-signup.html')
+
+# Admin Home Screen
+@app.route('/admin-home')
+def admin_home():
+  return render_template('admin-home.html', name=user_name)
+
+# Student Login Screen
+@app.route('/student-login', methods=['POST', 'GET'])
+def student_login():
+  if (request.method == 'POST'):
+    email = request.form['email']
+    password = request.form['password']
+    name = check_json(email, password, filename='students.json')
+    if (len(name) != 0):
+      global student_email
+      student_email = email
+      global student_password
+      student_password = password
+      global student_name
+      student_name = name
+      return redirect('/student-home')
+    else:
+      return render_template('student-login.html', incorrect=True)
+  else:
+    return render_template('student-login.html')
+
+# Student Signup Screen
+@app.route('/student-signup', methods=['POST', 'GET'])
+def student_signUp():
+  if (request.method == 'POST'):
+    data = {'email': request.form['email'], 
+            'password': request.form['password'],
+            'name': request.form['name'],
+            'points': 0}
+    
+    if (len(check_json(data['email'], data['password'], filename='students.json')) != 0):
+      return render_template('student-signup.html', repeat=True)
+    else:
+      write_json(data, filename='students.json')
+      return redirect('/student-login')
+  else:
+    return render_template('student-signup.html')
 
 ####################### Helper Methods #######################
   
-def write_json(new_data, filename='data.json'):
+def write_json(new_data, filename='admin.json'):
   with open(filename, 'r+') as file:
     file_data = json.load(file)
     file_data['users'].append(new_data)
     file.seek(0)
     json.dump(file_data, file, indent=4)
     
-def check_json(email, password, filename='data.json'):
+def check_json(email, password, filename='admin.json'):
   with open(filename, 'r+') as file:
     file_data = json.load(file)
     user_data = file_data['users']
